bb018cb6c35537c7f9164afa12b18354de9b0010
prakash <www.prak.co.in@gmail.com>
2012-12-06 23:50:50 -0800

    Create print_last_10_lines_of_file.c
    Given some text lines in one string, each line is separated by ‘\n’ character. Print the last ten lines. If number of lines is less than 10, then print all lines.

---
+++ print_last_10_lines_of_file.c
@@ -0,0 +1,6 @@
+/*
+    Given some text lines in one string, each line is separated by ‘\n’ character.
+    Print the last ten lines. If number of lines is less than 10, then print all lines.
+    For help 
+        http://www.geeksforgeeks.org/archives/28199
+*/
