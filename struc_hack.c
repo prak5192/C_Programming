3a16d8b6765ccf454a94e33ff8a7fd38fafbe135
prakash <www.prak.co.in@gmail.com>
2012-12-06 23:58:25 -0800

    Create struc_hack.c
    Great example of struc hack

---
+++ struc_hack.c
@@ -0,0 +1,19 @@
+/* Struct hack technique */
+
+/*
+    http://www.geeksforgeeks.org/archives/22677
+    
+*/
+
+
+struct employee
+{
+    int     emp_id;
+    int     name_len;
+    char    name[0];
+};
+
+int main(void){
+    printf("Sizeof struct employee is %d\n", sizeof(struct employee))
+    return 0;
+}
