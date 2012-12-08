263c4a5ffcb153c51ad9e3551befe9da65e64d44
U-Prak-PC\Prak <Prak@Prak-PC.(none)>
2012-12-09 01:29:41 +0530

    Completed the  answer

--- print_last_10_lines_of_file.c
+++ print_last_10_lines_of_file.c
@@ -4,3 +4,53 @@
     For help 
         http://www.geeksforgeeks.org/archives/28199
 */
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+#define PASS    0
+#define FAIL    -1
+
+int main(void){
+    FILE *fd = NULL;
+    int number = 10;
+    int ret = PASS;
+    char c = 'p';
+
+    fd = fopen("text.txt","r");
+    if(fd == NULL){
+        printf("Error in opening the file\n");
+        exit(0);
+    } else {
+        printf("Successfully opened the file\n");
+    }
+
+    ret = fseek(fd,0,SEEK_END);
+    if(ret == PASS){
+        printf("File Discriptor reached to the end of the file\n");
+    } else {
+        printf("Problem in fseek operation\n");
+    } 
+    while (( number != 0 ) || ((ret = ftell(fd)) == 0)){
+        fseek(fd,-2,SEEK_CUR);
+        c = fgetc(fd);
+        printf("%c",c);
+        if(c == '\n'){
+            number--;
+        }
+    }
+    ret = ftell(fd);
+    printf("Fd current position = %d\n",ret);
+    c = 'p';
+    while(c != EOF){
+        c = fgetc(fd);
+        printf("%c",c);
+    }
+         
+    return 0;
+}
+
+
+    
