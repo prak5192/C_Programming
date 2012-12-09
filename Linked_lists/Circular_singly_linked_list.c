ebe3a1600ae0e996be89930a94b8fb29439e9957
U-Prak-PC\Prak <Prak@Prak-PC.(none)>
2012-12-09 19:24:25 +0530

    Added the basic structure

--- Linked_lists/Circular_singly_linked_list.c
+++ Linked_lists/Circular_singly_linked_list.c
@@ -1,3 +1,24 @@
 /*
     Circular singly linked list
 */
+
+#include<stdio.h>
+#include<stdlib.h>
+
+#define PASS 0
+#define FAIL -1
+
+
+struct my_list{
+    int data;
+    struct my_list *next;
+};
+
+
+
+int main(void){
+    int ret = PASS;
+
+
+    return ret;
+}
