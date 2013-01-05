e45632848a9f95d8f29d07dbe9fd04da6955fd94
prak <prak@prak.(none)>
2013-01-05 20:41:51 +0530

    upadeted program

--- Good_Programs/My_programs/sum_natural_numbers.c
+++ Good_Programs/My_programs/sum_natural_numbers.c
@@ -5,10 +5,19 @@
 #include<stdio.h>
 
 int main(void){
+	int i = 0;
+	int n = 0;
+	int sum = 0;
+	printf("Enter the number till you want to calculate sum\n");
+	scanf("%d",&n);
+	for(i = 1; i<=n ; i++){
+		sum = sum + i;
+	}
+	printf("The sum calculated using simple for loop is %d\n",sum);
 
-
-
-
+	/* TODO: Calculate using recusrion */
+	sum = 0;
+	i = 0;
 
 	return 0;
 }
