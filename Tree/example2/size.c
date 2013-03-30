64280ceb114036a51391012e6fd94c140fa83a8f
Prak <prak@ubuntu.(none)>
2013-03-30 19:08:14 +0530

    added random number entry

--- Tree/example2/size.c
+++ Tree/example2/size.c
@@ -63,7 +63,7 @@ int size(struct node* p) {
     if(p!=NULL)
     {   
         inorder(p->left);
-        printf("Data :%d\n",p->data);
+       // printf("Data :%d\n",p->data);
         count ++;
         inorder(p->right);
     }   
@@ -81,7 +81,8 @@ int main(void){
     int i;
         
     for( i = 1; i <= 10 ; i++){
-        node = insert(node,i);     
+        int data = rand()/100;
+        node = insert(node,data);     
     }
     inorder(node);  
     printf("The size of the tree is %d\n", count);
