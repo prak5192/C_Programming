e2d138aab036be509a7df3757006a2cd0c8aed4d
Prak <www.prak.co.in@gmail.com>
2013-05-22 07:38:54 +0530

    Counting bit program

---
+++ Good_Programs/My_programs/counting_bits.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(void){
+    unsigned int x  = 10;
+	
+	x = (x&0x5555555) + ((x>>1)&0x55555555);
+	x = (x&0x3333333) + ((x>>2)&0x33333333);
+	x = (x&0x0f0f0f0f)  + ((x>>4)&0x0f0f0f0f);
+	x = (x&0x00ff00ff)  +  ((x>>8)&0x00ff00ff);
+	x = (x&0x0000ffff) + ((x>>16)&0x0000ffff);
+
+	printf("The number of set bits in x is %d\n",x);
+
+	return 0;
+}
+
