a2d81181f1fbf51f330d516fdc686a3162dd818a
Prak <www.prak.co.in@gmail.com>
2013-06-03 23:09:43 +0530

    Have to write this linked list

---
+++ Linked_lists/xor_linked_list.c
@@ -0,0 +1,10 @@
+#if 0
+Link:
+===========
+Implement a double linked list using a single linked list. 
+You have given only data and one pointer in struct node 
+{ int data, struct node *ptr}. 
+You should be able to traverse in both directions.
+http://en.wikipedia.org/wiki/XOR_linked_list
+
+#endif
